e673f24e936bca02e9f5ec4023760fea8740a51e
sharjeel0011 <caryzoon@gmail.com>
2024-12-07 23:31:38 +0500

    update navbar or linking

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,30 +1,85 @@
-import React from 'react'
+// import React from 'react'
+
+// const Navbar = () => {
+//   return (
+//   <>
+//   <div className="navbar bg-blue-900 ">
+//   <div className="flex-1">
+//     <a className="btn btn-ghost text-xl text-white">Logo</a>
+//   </div>
+//   <div className="flex-none">
+//     <ul className="menu menu-horizontal px-1">
+//     <li className='text-white'><a>Home</a></li>
+//       <li>
+//         <details>
+//           <summary className='text-white'>Parent</summary>
+//           <ul className="bg-base-100 rounded-t-none p-2">
+//             <li><a>Link 1</a></li>
+//             <li><a>Link 2</a></li>
+//           </ul>
+//         </details>
+//       </li>
+//       <li className='text-white'><a>Apply</a></li>
+//     </ul>
+//   </div>
+// </div>
+// </>
+//   )
+// }
+
+// export default Navbar
+
+
+
+
+
+
+
+
+
+
+
+import React from 'react';
+import { Link } from 'react-router-dom'; // Import Link component from react-router-dom
 
 const Navbar = () => {
   return (
-  <>
-  <div className="navbar bg-blue-900 ">
-  <div className="flex-1">
-    <a className="btn btn-ghost text-xl text-white">Logo</a>
-  </div>
-  <div className="flex-none">
-    <ul className="menu menu-horizontal px-1">
-   
-      <li>
-        <details>
-          <summary className='text-white'>Parent</summary>
-          <ul className="bg-base-100 rounded-t-none p-2">
-            <li><a>Link 1</a></li>
-            <li><a>Link 2</a></li>
+    <>
+      <div className="navbar bg-blue-900 ">
+        <div className="flex-1">
+          <Link to="/" className="btn btn-ghost text-xl text-white">Logo</Link>
+        </div>
+        <div className="flex-none">
+          <ul className="menu menu-horizontal px-1">
+            <li className="text-white">
+              <Link to="/">Home</Link> {/* Link to Home route */}
+            </li>
+            <li className="text-white">
+              <Link to="/Studentportal">Student Portal</Link> {/* Link to StudentPortal route */}
+            </li>
+            <li className="text-white">
+              <Link to="/coursfee">Fee Quarter Portal</Link> {/* Link to StudentPortal route */}
+            </li>
+            <li className="text-white">
+              <Link to="/Signup">Sign Up</Link> {/* Link to Signup route */}
+            </li>
+            <li className="text-white">
+              <Link to="/login">Login</Link> {/* Link to Login route */}
+            </li>
+            <li>
+              <details>
+                <summary className="text-white">Parent</summary>
+                <ul className="bg-base-100 rounded-t-none p-2">
+                  <li><Link to="/link1">Link 1</Link></li> {/* Example Link */}
+                  <li><Link to="/link2">Link 2</Link></li> {/* Example Link */}
+                </ul>
+              </details>
+            </li>
           </ul>
-        </details>
-      </li>
-      <li className='text-white'><a>Apply</a></li>
-    </ul>
-  </div>
-</div>
-</>
-  )
-}
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Navbar
+export default Navbar;
--- src/Config/RouterConfig/Routers.jsx
+++ src/Config/RouterConfig/Routers.jsx
@@ -7,6 +7,7 @@ import Navbar from '../../Components/Navbar';
 import Footer from '../../Components/Footer';
 import StudentPortal from '../../Screens/StudentPortal/StudentPortal';
 import Protected from '../../Components/StudentPortalPage/Protected';
+import Coursfee from '../../Components/StudentPortalPage/CoursFee'
 const Router = () => {
   return (
 <>
@@ -15,6 +16,7 @@ const Router = () => {
 <Routes>
 <Route path="/" element={<Home />} />
 <Route path='Studentportal' element={<Protected><StudentPortal/></Protected>} />
+<Route path='coursfee' element={<Protected><Coursfee/></Protected>} />
 <Route path='Signup' element={<StudentSignUpScreen/>} />
 <Route path='login' element={<LoginScreen/>} />
 </Routes>
