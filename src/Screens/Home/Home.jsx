fdf4eb3e6bfdedd9421c77b0d4076c85782766ab
sharjeel0011 <caryzoon@gmail.com>
2024-12-10 20:04:58 +0500

    final udete 1

--- src/Screens/Home/Home.jsx
+++ src/Screens/Home/Home.jsx
@@ -1,28 +1,3 @@
-// import React from 'react'
-// import HeroSection from '../../Components/HomePage/Hero'
-// import AvailablePrograms from '../../Components/HomePage/AvailabelPrograms'
-// import YouTubeSeries from '../../Components/HomePage/YoutubeSeries'
-// const Home = () => {
-//   return (
-// <>
-// <div className=''>
-// <HeroSection/>
-// <AvailablePrograms/>
-// <YouTubeSeries/>
-// </div>
-// </>
-//   )
-// }
-
-// export default Home
-
-
-
-
-
-
-
-
 
 
 import React from "react";
